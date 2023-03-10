0a38d28cdd52682ea6eaad6290f7690f98e6070e
Doonske <kerndaniel.08@gmail.com>
2023-03-10 09:55:09 +0100

    Änderung - nur interest count zurückgeben

--- routes/interest.js
+++ routes/interest.js
@@ -3,51 +3,10 @@ const router = express.Router();
 const entries = require("../services/entries");
 const interestService = require("../services/interest");
 
-// POST-Methode, um einen Interessenten für ein Objekt zu registrieren
-router.post('/interests', async function(req, res, next) {
-    const { user_id, object_id } = req.body;
-  
-    // Überprüfen, ob der Benutzer und das Objekt vorhanden sind
-    const user = await db.query('SELECT id FROM users WHERE id = ?', [user_id]);
-    const object = await db.query('SELECT id, interests FROM objects WHERE id = ?', [object_id]);
-    if (!user || !object) {
-      const error = new Error('User or object not found');
-      error.statusCode = 404;
-      throw error;
-    }
-  
-    // Überprüfen, ob der Benutzer bereits als Interessent registriert ist
-    const interest = await db.query('SELECT id FROM interests WHERE user_id = ? AND object_id = ?', [user_id, object_id]);
-    if (interest) {
-      const error = new Error('User already registered as interested in this object');
-      error.statusCode = 400;
-      throw error;
-    }
-  
-    // Eintrag in der interests-Tabelle erstellen
-    await db.query('INSERT INTO interests (user_id, object_id) VALUES (?, ?)', [user_id, object_id]);
-  
-    // Anzahl der Interessenten in der objects-Tabelle aktualisieren
-    const interests = object[0].interests + 1;
-    await db.query('UPDATE objects SET interests = ? WHERE id = ?', [interests, object_id]);
-  
-    res.json({ message: 'User registered as interested in object' });
-  });
-  
-  // GET request to get the number of interests
-/*router.get("/entries/:id/interest", async (req, res, next) => {
-  try {
-    //res.json(await entries.getMultiple(req.query.page));
-    //const count = await interestService.getInterestCount(req.params.id);
-    res.json(await interestService.getInterestCount(req.params.id));
-  } catch (err) {
-    next(err);
-  }
-});*/
-
+// Anforderung: Für ein einzelnes Objekt können Sie sich nur die Anzahl der Interessenten ausgeben
 router.get("/:id", async function (req, res) {
   try {
-  res.json(await interestService.getInterestCount(req.params.id));
+  res.json(await interestService.getInterestById(req.params.id));
   } catch (err) {
     if (err.code === 'ER_BAD_FIELD_ERROR') {
       resolveErrors(res, 'Missing property in request', 400)
@@ -60,7 +19,7 @@ router.get("/:id", async function (req, res) {
   }
 });
   
-// POST request to add a new interest
+// POST-Methode, um einen Interessenten für ein Objekt zu registrieren
 router.post("/:id", async function (req, res){
   try {
     res.json(await interestService.addInterest(req.params.id));
