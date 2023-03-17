42703ba0f204408383e781ff85849903aba5a928
Julee1604 <julian.leicht@gmx.de>
2023-03-17 17:58:24 +0100

    Frontend kleine Anpassungen

--- Client/src/components/EntryList.js
+++ Client/src/components/EntryList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../index.css';
 
 function EntryList() {
 
@@ -17,27 +18,31 @@ apiCall();
 
 return (
   <div className='App'>
-  <header className='App-header'>
-  <h1>All Entries</h1>
-  {entries.length > 0 ?
-  <ul>
-    {entries.map(entry => (
-    <li key={entry.id}>
-    <p>Created On: {entry.createdOn}</p>
-    <p>Created By: {entry.createdBy}</p>
-    <p>Software Version: {entry.softwareVersion}</p>
-    <p>Customer: {entry.customer}</p>
-    <p>Entry Type: {entry.entry_type}</p>
-    <p>Address: {entry.entry_address}</p>
-    <p>Size: {entry.entry_size}</p>
-    <p>Comment: {entry.entry_comment}</p>
-    <p>Short Hand: {entry.entry_shortHand}</p>
-    </li>
-    ))}
-  </ul> :
-  <p>Loading...</p>
-  }
-  </header>
+    <header className='App-header'>
+      <h1>All Entries</h1>
+      {entries.length > 0 ?
+        <ul className='entry-list'>
+
+          {entries.map(entry => (
+            <li key={entry.id} className='entry-item'>
+              <div className='entry-box'>
+                <p className='entry-text'>Created On: {entry.createdOn}</p>
+                <p className='entry-text'>Created By: {entry.createdBy}</p>
+                <p className='entry-text'>Software Version: {entry.softwareVersion}</p>
+                <p className='entry-text'>Customer: {entry.customer}</p>
+                <p className='entry-text'>Entry Type: {entry.entry_type}</p>
+                <p className='entry-text'>Address: {entry.entry_address}</p>
+                <p className='entry-text'>Size: {entry.entry_size}</p>
+                <p className='entry-text'>Comment: {entry.entry_comment}</p>
+                <p className='entry-text'>Short Hand: {entry.entry_shortHand}</p>
+
+              </div>
+            </li>
+          ))}
+        </ul> :
+        <p>Loading...</p>
+      }
+    </header>
   </div>
 );
 
