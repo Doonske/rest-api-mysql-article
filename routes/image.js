5fb7ada966534b7c375db8c412a5ea4005ffb408
Doonske <kerndaniel.08@gmail.com>
2023-03-17 15:22:50 +0100

    Bild Upload funktioniert

--- routes/image.js
+++ routes/image.js
@@ -1,29 +1,87 @@
 const express = require("express");
 const router = express.Router();
 const images = require("../services/image");
-// POST-Methode zum Hochladen eines Bildes für ein Objekt
+const path = require('path');
+const fs = require('fs');
+const config = require("../config");
+const db = require("../services/db")
 
 const multer = require('multer');
+
+// Beispiel-Pfad zum öffentlichen Ordner, in dem das Bild gespeichert ist
+const publicDir = path.join(__dirname, '..', '');
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
   },
-  filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}.${file.originalname.split('.').pop()}`)
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
   }
 });
+
 const upload = multer({ storage: storage });
 
-router.post("/:id/upload-image", upload.single('image'), async function (req, res){
-  try {
-    // Hier kann der Pfad zum Bild in der Datenbank gespeichert werden
-    const imagePath = `uploads/${req.file.filename}`;
-    // ...
+const logger = (req, res, next) => {
+  console.log(`Received Request ${new Date(Date.now()).toLocaleString('de-DE')}`);
+  console.log('HTTP METHOD', req.method);
+  console.log('HTTP BODY', req.body);
+  console.log('URL PARAMETER', req.params);
+  next();
+}
+router.use(logger);
+
+router.post('/:id', upload.single('image'), async (req, res) => {
+  const id = req.params.id;
+  const imagePath = req.file.path;
+  const result = await db.query('UPDATE entries SET image_path = ? WHERE id = ?', [imagePath, id]);
+
+  if (result.affectedRows) {
     res.json({ success: true });
-  } catch (err) {
-    // ...
+  } else {
+    res.json({ success: false });
   }
 });
 
-module.exports = router;
+/*router.get("/:id", (req, res) => {
+  const imageId = req.params.id;
+  const imagePath = path.join(publicDir, `${imageId}.jpg`); // Annahme, dass das Bild im JPEG-Format ist
 
+  // Überprüfen, ob die Datei existiert
+  fs.access(imagePath, fs.F_OK, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('File not found');
+      return;
+    }
+
+    // Bild wird als Antwort gesendetr
+    res.sendFile(imagePath);
+  });
+});*/
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const result = await db.query('SELECT image_path FROM entries WHERE id = ?', [id]);
+  
+  if (result.length) {
+  const imagePath = result[0].image_path;
+  const absolutePath = path.join(publicDir, imagePath);
+  
+  // Überprüfen, ob die Datei existiert
+  fs.access(absolutePath, fs.F_OK, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('File not found');
+      return;
+    }
+  
+    // Bild wird als Antwort gesendet
+    res.sendFile(absolutePath);
+  });
+  
+  } else {
+  res.status(404).send('Entry not found');
+  }
+  });
+module.exports = router;
